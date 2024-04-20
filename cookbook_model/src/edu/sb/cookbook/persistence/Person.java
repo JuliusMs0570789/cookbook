a9a113cd4b419508210edd417f80df084c3a852f
Julius Schultz <julius.schultz@gmx.de>
2024-04-20 12:07:30 +0200

    First commit

---
+++ cookbook_model/src/edu/sb/cookbook/persistence/Person.java
@@ -0,0 +1,7 @@
+package edu.sb.cookbook.persistence;
+
+public class Person {
+	static public enum category {
+		MAIN_COURSE, APPETIZER, SNACK, DESSERT, BREAKFAST, BUFFET, BARBEQUE, ADOLESCENT, INFANT		
+	}
+}
