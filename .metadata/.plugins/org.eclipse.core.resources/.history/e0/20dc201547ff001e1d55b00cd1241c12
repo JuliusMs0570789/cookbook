6454a03c4171156e718f40a13ce0168f96ff2f2b
Julius Schultz <julius.schultz@gmx.de>
2024-04-20 20:57:38 +0200

    work on Recipe.java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e0/20dc201547ff001e1d55b00cd1241c12
@@ -0,0 +1,7 @@
+package edu.sb.cookbook.persistence;
+
+public class Ingredient {
+	static public enum unit {
+		LITRE, GRAM, TEASPOON, TABLESPOON, PINCH, CUP, CAN, TUBE, BUSHEL, PIECE		
+	}
+}
--- cookbook_model/src/edu/sb/cookbook/persistence/Ingredient.java
+++ cookbook_model/src/edu/sb/cookbook/persistence/Ingredient.java
@@ -1,7 +1,42 @@
 package edu.sb.cookbook.persistence;
 
-public class Ingredient {
+import javax.json.bind.annotation.JsonbProperty;
+import javax.persistence.Column;
+
+public class Ingredient {	
+	@Column(nullable=false, updatable=true)
+	private Recipe recipe;
+	
+	private IngredientType type;
+	
+	private float amount;
+	
 	static public enum unit {
 		LITRE, GRAM, TEASPOON, TABLESPOON, PINCH, CUP, CAN, TUBE, BUSHEL, PIECE		
+	}
+	
+	public Ingredient () {
+		// TODO: how can the recipe be initialized at first?
+		// this.recipe = ...;
+		this.type = null;
+		this.amount = 0;
+	}
+	
+	@JsonbProperty
+	public Recipe getRecipe () {
+		return this.recipe;
+	}
+	
+	protected void setRecipe (final Recipe recipe) {
+		this.recipe = recipe;
+	}
+	
+	@JsonbProperty
+	public IngredientType getType () {
+		return this.type;
+	}
+	
+	public void setType (final IngredientType type) {
+		this.type = type;
 	}
 }
