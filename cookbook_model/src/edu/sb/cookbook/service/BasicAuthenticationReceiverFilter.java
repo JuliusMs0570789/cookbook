d8876087fc855ecf5ba197040706b2190e3adab3
lukasGlas <58211068+lukasGlas@users.noreply.github.com>
2024-05-14 20:05:39 +0200

    PersonService & AuthenticationFilter filtering added

--- cookbook_model/src/edu/sb/cookbook/service/BasicAuthenticationReceiverFilter.java
+++ cookbook_model/src/edu/sb/cookbook/service/BasicAuthenticationReceiverFilter.java
@@ -4,13 +4,20 @@ import static javax.ws.rs.core.HttpHeaders.WWW_AUTHENTICATE;
 import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 // import java.util.Base64;
 import javax.annotation.Priority;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+
+import edu.sb.cookbook.persistence.Person;
 import edu.sb.tool.Copyright;
+import edu.sb.tool.HashCodes;
 
 
 /**
@@ -26,6 +33,9 @@ public class BasicAuthenticationReceiverFilter implements ContainerRequestFilter
 	 * HTTP request header for the authenticated requester's identity.
 	 */
 	static public final String REQUESTER_IDENTITY = "X-Requester-Identity";
+	
+	 @PersistenceContext
+	    private EntityManager entityManager;
 
 
 	/**
@@ -39,27 +49,71 @@ public class BasicAuthenticationReceiverFilter implements ContainerRequestFilter
 	 *         "Requester-Identity" header
 	 */
 	public void filter (final ContainerRequestContext requestContext) throws NullPointerException, ClientErrorException {
-		// TODO:
 		// - Throw a ClientErrorException(Status.BAD_REQUEST) if the given context's headers map already contains a
 		//   "Requester-Identity" key, in order to prevent spoofing attacks.
+		if (requestContext.getHeaders().containsKey(REQUESTER_IDENTITY)) {
+			throw new ClientErrorException(UNAUTHORIZED);
+		}
+
 		// - Remove the "Authorization" header from said map and store the first of it's values in a variable
 		//   "textCredentials", or null if the header value is either null or empty.
+		String authHeader = requestContext.getHeaderString("Authorization");
+		if (authHeader == null || authHeader.isEmpty()) {
+			throw new ClientErrorException(UNAUTHORIZED);
+		}
+
 		// - if the "textCredentials" variable is not null, parse it programmatically using Base64.getDecoder().decode(),
 		//   use the resulting byte array to create a new String instance, and store the resulting <email>:<password>
 		//   combination in variable "credentials". 
+		String[] authParts = authHeader.split(" ");
+		if (authParts.length != 2 || !authParts[0].equalsIgnoreCase("Basic")) {
+			throw new ClientErrorException(UNAUTHORIZED);
+		}
+		String credentials = new String(java.util.Base64.getDecoder().decode(authParts[1]));
+		String[] parts = credentials.split(":");
+		if (parts.length != 2) {
+			throw new ClientErrorException(UNAUTHORIZED);
+		}
+		String email = parts[0];
+		String password = parts[1];
+
 		// - Perform the PQL-Query "select p from Person as p where p.email = :email"), using the credentials email
 		//   address part. Note that this query will go to the second level cache before hitting the database if the
 		//   Person#email field is annotated using @CacheIndex(updateable = true)! 
-		// - if the resulting people list contains exactly one element, calculate the hex-string representation
-		//   (i.e. 2 digits per byte) of the SHA2-256 hash code of the credential's password part using
-		//   HashCodes.sha2HashText(256, text).
-		// - if this hash representation is equal to queried person's password hash, add a new "Requester-Identity"
-		//   header to the request headers, using the person's identity (converted to String) as value, and return
-		//   from this method.
-		// - in all other cases, abort the request using requestContext.abortWith() in order to challenging the client
+		TypedQuery<Person> query = entityManager.createQuery("SELECT p FROM Person p WHERE p.email = :email", Person.class);
+		query.setParameter("email", email);
+
+		try {
+		    Person person = query.getSingleResult();
+            
+    		// - if the resulting people list contains exactly one element, calculate the hex-string representation
+    		//   (i.e. 2 digits per byte) of the SHA2-256 hash code of the credential's password part using
+    		//   HashCodes.sha2HashText(256, text).
+            String hashedPassword = HashCodes.sha2HashText(256, password);
+
+    		// - if this hash representation is equal to queried person's password hash, add a new "Requester-Identity"
+    		//   header to the request headers, using the person's identity (converted to String) as value, and return
+    		//   from this method.
+            if (hashedPassword.equals(person.getPasswordHash())) {
+                // Authentication successful, add Requester-Identity header
+                requestContext.getHeaders().add(REQUESTER_IDENTITY, Long.toString(person.getIdentity()));
+                return;
+            }
+        } catch (NoResultException e) {
+            // Person not found
+        }
+
+    	// - in all other cases, abort the request using requestContext.abortWith() in order to challenging the client
 		//   to provide HTTP Basic credentials (i.e. status code 401, and "WWW-Authenticate" header value "Basic").
 		//   Note that the alternative of throwing NotAuthorizedException("Basic") comes with the disadvantage that
 		//   failed authentication attempts clutter the server log with stack traces.
-		requestContext.abortWith(Response.status(UNAUTHORIZED).header(WWW_AUTHENTICATE, "Basic").build());
+        requestContext.abortWith(Response.status(UNAUTHORIZED).header(WWW_AUTHENTICATE, "Basic").build());
+		// TODO:
+	
+		
+		
+
+
+	
 	}
 }
